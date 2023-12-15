1d262245cbb4eea580d726f8ce8c52ddaabc26d7
Zack Stevens <zack.st7@gmail.com>
2023-12-15 14:24:45 -0700

    feat!: Refactor to use generic methods

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"github.com/catalystsquad/app-utils-go/env"
+	gorm "github.com/catalystsquad/protoc-gen-go-gorm/options"
 	"github.com/catalystsquad/protoc-gen-go-gorm/plugin"
 	"github.com/golang/glog"
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/descriptorpb"
 	"google.golang.org/protobuf/types/pluginpb"
+	"reflect"
 )
 
 var logLevel = env.GetEnvOrDefault("LOG_LEVEL", "ERROR")
@@ -34,14 +38,38 @@ func main() {
 
 			gf := gp.NewGeneratedFile(fmt.Sprintf("%s.pb.gorm.go", f.GeneratedFilenamePrefix), f.GoImportPath)
 
-			err := plugin.ApplyTemplate(gf, f)
-			if err != nil {
-				gf.Skip()
-				gp.Error(err)
-				continue
+			if shouldGenerateFile(f) {
+				err := plugin.ApplyTemplate(gf, f)
+				if err != nil {
+					gf.Skip()
+					gp.Error(err)
+					continue
+				}
 			}
+
 		}
 
 		return nil
 	})
+}
+
+func shouldGenerateFile(file *protogen.File) bool {
+	options := getFileOptions(file)
+	return options != nil && options.Generate
+}
+
+func getFileOptions(file *protogen.File) *gorm.GormFileOptions {
+	options := file.Desc.Options().(*descriptorpb.FileOptions)
+	if options == nil {
+		return &gorm.GormFileOptions{}
+	}
+	v := proto.GetExtension(options, gorm.E_FileOpts)
+	if reflect.ValueOf(v).IsNil() {
+		return nil
+	}
+	opts, ok := v.(*gorm.GormFileOptions)
+	if !ok {
+		return nil
+	}
+	return opts
 }
