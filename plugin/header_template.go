448d7a90e05b25b72500a428ab6b274200501bdd
Zack Stevens <zack.st7@gmail.com>
2023-12-19 11:19:54 -0700

    feat!: Refactor to use generic methods

--- plugin/header_template.go
+++ plugin/header_template.go
@@ -7,6 +7,4 @@ var headerTemplate = template.Must(template.New("header").Parse(`
 // source: {{ .Proto.Name }}
 
 package {{ .GoPackageName }}
-
-const TimestampFormat = "2006-01-02T15:04:05.999999Z07:00"
 `))
